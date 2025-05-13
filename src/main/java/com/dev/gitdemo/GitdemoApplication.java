e700fa0a775732f899cb00391d1252902cd3ed75
Devavrat77 <devavratptl23@gmail.com>
2025-05-13 21:15:50 +0530

    Dev 2

--- src/main/java/com/dev/gitdemo/GitdemoApplication.java
+++ src/main/java/com/dev/gitdemo/GitdemoApplication.java
@@ -8,7 +8,7 @@ public class GitdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitdemoApplication.class, args);
-		System.out.println("Devavrat");
+		System.out.println("Devavrat 2");
 	}
 
 }
